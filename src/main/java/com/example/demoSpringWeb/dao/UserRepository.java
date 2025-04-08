2169c14de6c985cadb53e9ede84fec67cda4cb47
Ramana-1234 <142676843+Ramana-1234@users.noreply.github.com>
2025-04-08 08:08:49 +0530

    Update UserRepository.java

--- src/main/java/com/example/demoSpringWeb/dao/UserRepository.java
+++ src/main/java/com/example/demoSpringWeb/dao/UserRepository.java
@@ -1,1 +1,8 @@
+package com.example.demoSpringWeb.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demospringweb.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity,Integer>{
+	UserEntity findByusernameAndpassword(String username, String password);
+}
