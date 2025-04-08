b0f6da72dace89f0e77b359d8ba5fe17716a73cc
Ramana-1234 <142676843+Ramana-1234@users.noreply.github.com>
2025-04-08 08:01:59 +0530

    Update UserAccountDetailsEntity.java

--- src/main/java/com/example/demoSpringWeb/entity/UserAccountDetailsEntity.java
+++ src/main/java/com/example/demoSpringWeb/entity/UserAccountDetailsEntity.java
@@ -1,32 +1,41 @@
 package com.example.demoSpringWeb.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="user_account_details")
+@Table(name = "user_account_details")
 public class UserAccountDetailsEntity {
-	
 	@Id
-	@Column(name="user_account_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	@Column(name = "user_account_id")
 	private int userAccountId;
 	
-	@Column(name="user_id")
-	private int userId;
+	@OneToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+	private UserEntity userEntity;
 	
-	@Column(name="account_open_date")
-	private String accountOpenDate;
+	@Column(name = "account_open_date", nullable = false)
+	private LocalDate accountOpenDate;
 	
-	@Column(name="current_wallet_balance")
-	private double currentWalletBalance;
+	@Column(name = "account_close_date")
+	private LocalDate accountCloseDate;
 	
-	@Column(name="linked_bank_accounts_count")
-	private int linkedBankAccountsCount;
+	@Column(name = "current_wallet_balance", nullable = false)
+	private BigDecimal currentWalletBalance = BigDecimal.ZERO; 
 	
-	@Column(name="wallet_pin")
+	@Column(name = "linked_bank_accounts_count", nullable = false)
+	private int linkedBankAccountsCount = 0;  
+	
+	@Column(name = "wallet_pin", nullable = false)
 	private String walletPin;
-	
-
 }
