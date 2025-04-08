c9b57b0bbe9c41bb17d4c91338c8b183446ce8ac
Ramana-1234 <142676843+Ramana-1234@users.noreply.github.com>
2025-04-08 08:03:22 +0530

    Update BankAccountsEntity.java

--- src/main/java/com/example/demoSpringWeb/entity/BankAccountsEntity.java
+++ src/main/java/com/example/demoSpringWeb/entity/BankAccountsEntity.java
@@ -1,5 +1,43 @@
 package com.example.demoSpringWeb.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="bank_accounts")
 public class BankAccountsEntity {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bank_account_id")
+    private int bankAccountId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_account_id", referencedColumnName = "user_account_id", nullable = false)
+    private UserAccountDetailsEntity userAccountDetailsEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+    private UserEntity userEntity;
+
+    @Column(name = "account_number", nullable = false)
+    private String accountNumber;
+
+    @Column(name = "ifsc_code", nullable = false)
+    private String ifscCode;
+
+    @Column(name = "bank_name", nullable = false)
+    private String bankName;
+
+    @Column(name = "bank_account_branch_location", nullable = false)
+    private String bankAccountBranchLocation;
+
+    @Column(name = "is_active", nullable = false)
+    private String isActive;
 
 }
